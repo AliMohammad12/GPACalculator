cfcfa8640ec41626d3b66e2724f07b51a97f5655
Ali Mohammad <Aliwasfi12321@gmail.com>
2023-08-14 09:49:02 +0300

    GPACalculator Service now communicates with AuthenticationService and fixe docker-compose.yml

--- GPACalculatorService/src/main/java/application/gpacalculatorservice/authentication/AuthenticationService.java
+++ GPACalculatorService/src/main/java/application/gpacalculatorservice/authentication/AuthenticationService.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AuthenticationService {
     private final RestTemplate restTemplate = new RestTemplate();
-    private final String authUrl = "http://localhost:8082/auth/authenticate";
+    private final String authUrl = "http://authentication:8082/auth/authenticate";
     public boolean authenticate(AuthenticationRequest authenticationRequest) {
         ResponseEntity<AuthenticationResponse> responseEntity = restTemplate.postForEntity(authUrl, authenticationRequest, AuthenticationResponse.class);
         return responseEntity.getBody().isAuthenticated();
